512e5084e773b3ce5f02d4177dea56634a642d22
RishiM27 <rishimohapatra270806@gmail.com>
2025-04-11 02:06:48 +0530

    Add files via upload

---
+++ wing_allot.py
@@ -0,0 +1,59 @@
+import pyautogui
+
+#Make sure pyautogui library is installed (pip3 install pyautogui)
+#specify the UIDs in the pyautogui.write() fields.
+'''Keep the SWD portal open and logged in on your browser
+Keep the wing allotment page open
+Select the hostel choice and wing size
+Activate the first UID field (Make sure the cursor is there)
+Run the program and open the browser within the sleep time mentioned in the below command.'''
+
+pyautogui.sleep(1)
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+ 
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
+
+pyautogui.write("")
+pyautogui.hotkey("tab")
